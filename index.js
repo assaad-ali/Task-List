f9187ebff1b85931fd2447e74821c6bba31c2d24
Ali-Assaad <aliassaadtr@gmail.com>
2023-02-22 16:25:28 +0200

    Add edit function

--- index.js
+++ index.js
@@ -44,5 +44,16 @@ window.addEventListener('load', () => {
 		list_el.appendChild(task_el);
 
 		input.value = '';
+
+		task_edit_el.addEventListener('click', (e) => {
+			if (task_edit_el.innerText.toLowerCase() == "edit") {
+				task_edit_el.innerText = "Save";
+				task_input_el.removeAttribute("readonly");
+				// task_input_el.focus();
+			} else {
+				task_edit_el.innerText = "Edit";
+				task_input_el.setAttribute("readonly", "readonly");
+			}
+		});
 	});
 })
