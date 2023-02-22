095f68e311e272c574ebed035244bb9ea7d071bb
Ali-Assaad <aliassaadtr@gmail.com>
2023-02-22 16:22:21 +0200

    Create task content element

--- index.js
+++ index.js
@@ -11,5 +11,10 @@ window.addEventListener('load', () => {
 
 		const task_el = document.createElement('div');
 		task_el.classList.add('task');
+
+		const task_content_el = document.createElement('div');
+		task_content_el.classList.add('content');
+
+		task_el.appendChild(task_content_el);
 	});
 })
