967627737a2b1c883bb15048bd4417042b95d295
Ali-Assaad <aliassaadtr@gmail.com>
2023-02-22 16:20:10 +0200

    Declare task on submit

--- index.js
+++ index.js
@@ -4,5 +4,9 @@ window.addEventListener('load', () => {
 	const input = document.querySelector("#new-task-input");
 	const list_el = document.querySelector("#tasks");
 
-	
+	form.addEventListener('submit', (e) => {
+		e.preventDefault();
+
+		const task = input.value;
+	});
 })
